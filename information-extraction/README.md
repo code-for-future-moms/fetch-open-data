70d82d6e151da199d659d8d2de0c14f35088fcc0
Mei Sasaki <october.sky.2009@gmail.com>
2023-09-06 10:40:58 +0900

    READMEをupdate

--- information-extraction/README.md
+++ information-extraction/README.md
@@ -1,6 +1,22 @@
 # information extraction
-- PDFファイルの取得
+
+## 前準備
+- 必要なライブラリのインストール
+```commandline
+brew install poppler
+pip install mojimoji
+```
+
+## 作業手順
+- PDFファイルの取得と`mydir`への配置
 ```sh
 wget -r -l 1 -nH -nd -np --ignore-case -A '*.pdf' https://www.fukushi.metro.tokyo.lg.jp/kodomo//kosodate/josei/funin/shiteiiryou-jouhoukoukai.html -P mydir
 ```
 - 情報の抽出
+  1. Poppler実行
+  1. テキストファイルから正規表現を用いて各種統計値を抽出・ファイルに出力
+  1. 正規表現では取りきれないので人力でレビュー・修正
+```commandline
+python run_poppler.py
+python create_output.py
+```
--- information-extraction/run_poppler.py
+++ information-extraction/run_poppler.py
@@ -3,8 +3,8 @@ import subprocess
 
 in_dir = "./mydir" # pdf files are located here
 out_dir = "./text"
-for filename in os.listdir(in_dir):
-    f = os.path.join(in_dir, filename)
-    pdf_name = filename.replace('.pdf', '.txt')
-    output_path = os.path.join(out_dir, pdf_name)
+for pdf_name in os.listdir(in_dir):
+    f = os.path.join(in_dir, pdf_name)
+    txt_name = pdf_name.replace('.pdf', '.txt')
+    output_path = os.path.join(out_dir, txt_name)
     subprocess.run(["pdftotext", "-raw", "-layout", "-nopgbrk", f, output_path])
