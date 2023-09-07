61d4aa6320604d77e25ab84eddf4c02fd5fa54e5
Mei Sasaki <october.sky.2009@gmail.com>
2023-09-07 12:40:22 +0900

    正規表現を精緻化, 妊娠率の一貫性チェックも追加

--- information-extraction/create_output.py
+++ information-extraction/create_output.py
@@ -9,7 +9,7 @@ import csv
 
 with open(medium_file, 'w', encoding="utf-8", newline='') as fout:
     writer = csv.writer(fout, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["name", "et_count", "preg_count", "birth_count", "birth_ratio", "url"])
+    writer.writerow(["name", "et_count", "preg_count", "birth_count", "birth_ratio", "url", "log"])
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     subprocess.run(["python", "extract_from_txt.py", f, medium_file])
--- information-extraction/extract_from_txt.py
+++ information-extraction/extract_from_txt.py
@@ -9,9 +9,14 @@ with open(args.input_path) as fin:
     text = "".join(fin.readlines())
 # print(text)
 import mojimoji
-
+# 数字を全角から半角に
 text = mojimoji.zen_to_han(text, kana=False, ascii=False)
+# 小数点を全角から半角に
 import re
+text = re.sub(r"(?<=\d)．(?=\d)", ".", text)
+
+# 全角/半角コンマは除去
+text = re.sub(r"(?<=\d)[，,](?=\d)", "", text)
 regex = re.compile(r"(?<=医療機関名：).+(?=\n)")
 matches = re.findall(regex, text)
 name = None
@@ -41,10 +46,17 @@ import csv
 with open(args.output_path, 'a', encoding="utf-8", newline='') as fout:
     writer = csv.writer(fout, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
     if name is not None and len(stats) == 4 and clinic_url is not None:
-        writer.writerow([name, stats[0], stats[1], stats[2], stats[3], clinic_url])
+        et_count = int(stats[0])
+        birth_count = int(stats[2])
+        birth_rate = float(stats[3])
+        birth_rate2 = birth_count/et_count * 100
+        if abs(birth_rate - birth_rate2) < 1:
+            writer.writerow([name, stats[0], stats[1], stats[2], stats[3], clinic_url, "VALID"])
+        else:
+            writer.writerow([name, stats[0], stats[1], stats[2], stats[3], clinic_url, "INCONSISTENT"])
     elif name is None:
-        writer.writerow(["NAME_NOT_IDENTIFIED", "", "", "", "", args.input_path])
+        writer.writerow(["", "", "", "", "", "", "NAME_NOT_IDENTIFIED:{}".format(args.input_path)])
     elif len(stats) != 4:
-        writer.writerow([name, "", "", "", "", "NUMBERS_NOT_EXTRACTED{}:{}".format(args.input_path, ",".join(stats))])
+        writer.writerow([name, "", "", "", "", "", "NUMBERS_NOT_EXTRACTED{}:{}".format(args.input_path, ",".join(stats))])
     elif clinic_url is None:
-        writer.writerow([name, stats[0], stats[1], stats[2], stats[3], "URL_NOT_EXTRACTED:{}".format(args.input_path)])
+        writer.writerow([name, stats[0], stats[1], stats[2], stats[3], "", "URL_NOT_EXTRACTED:{}".format(args.input_path)])
