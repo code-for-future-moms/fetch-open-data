c5a5a304cf4e617a6621b650b260a2d95d5f2e26
Mei Sasaki <october.sky.2009@gmail.com>
2023-12-13 10:53:09 +0900

    データ抽出スクリプト修正

--- information-extraction2/extract_urls.py
+++ information-extraction2/extract_urls.py
@@ -13,11 +13,10 @@ def get_hrefs(url, ids):
         elif a["href"].startswith(url + "&page="):
             get_hrefs(a["href"], ids)
 
-
+print('printf "name\taddress\tprefecture\tphone\tflag1\tflag2\tflag3\tflag4\tflag5\tflag6\tflat7\tflag8\tfrozen_egg\tfrozen_preg\tfrozen_birth\tfrozen_rate\tclinic_url\n"')
 for i in range(47):
     url = "https://funin-fuiku.cfa.go.jp/clinic/search/?area%5B%5D={}".format(i)
     ids = set()
     get_hrefs(url, ids)
     for id in ids:
         print("python extract.py https://funin-fuiku.cfa.go.jp/clinic/search/modal.php?id={}".format(id))
-
